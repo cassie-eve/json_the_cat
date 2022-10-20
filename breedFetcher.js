33ad0771f50838df4442946a03df9bc813c526bf
Cassandra Keddis <cassandra@thinkific.com>
2022-10-19 20:28:06 -0700

    included error messaging

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,20 @@
 const request = require('request');
+
+const breed = process.argv[2];
+const requestURL = `https://api.thecatapi.com/v1/breeds/search?name=${breed}`;
+
+request(requestURL, (error, response, body) => {
+
+  if (!breed) {
+    console.log(`Please include the name of the cat breed you would like to search for.`);
+    return;
+  }
+
+  const data = JSON.parse(body);
+
+  if (data.length === 0) {
+    console.log(`Please enter the name of a real cat breed`)
+  } else {
+    console.log(data[0].description);
+  }
+});
