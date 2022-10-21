77324130d5ae09522371aad0541be087ad40ca78
Cassandra Keddis <cassandra@thinkific.com>
2022-10-20 20:06:18 -0700

    refactor and addition of index

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,20 @@
 const request = require('request');
 
-const breed = process.argv[2];
-const requestURL = `https://api.thecatapi.com/v1/breeds/search?name=${breed}`;
+const fetchBreedDescription = function(breed, callback) {
+  const requestURL = `https://api.thecatapi.com/v1/breeds/search?name=${breed}`;
+  request(requestURL, (error, response, body) => {
+    if (error) {
+      return callback('Oh no:', error);
+    }
+  
+    const data = JSON.parse(body);
+  
+    if (data.length === 0) {
+      callback('Please enter the name of a real cat breed');
+    } else {
+      callback(null, data[0].description);
+    }
+  }); 
+};
 
-request(requestURL, (error, response, body) => {
-  if (error) {
-    console.log(`Oh no:`, error);
-  }
-  if (!breed) {
-    console.log(`Please include the name of the cat breed you would like to search for.`);
-    return;
-  }
-
-  const data = JSON.parse(body);
-
-  if (data.length === 0) {
-    console.log(`Please enter the name of a real cat breed`);
-  } else {
-    console.log(data[0].description);
-  }
-});
+module.exports = { fetchBreedDescription }
